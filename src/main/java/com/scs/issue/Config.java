ebb51593382e41b531e2d259257e910a81934eb2
Mantas Matūzas <m.matuzas@ba.lt>
2021-06-09 16:06:46 +0300

    use 1.0.8-SNAPSHOT reactor

--- src/main/java/com/scs/issue/Config.java
+++ src/main/java/com/scs/issue/Config.java
@@ -1,24 +1,43 @@
 package com.scs.issue;
 
+import brave.http.HttpTracing;
+import org.springframework.boot.web.embedded.netty.NettyServerCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.zalando.logbook.Logbook;
 import org.zalando.logbook.netty.LogbookClientHandler;
+import org.zalando.logbook.netty.LogbookServerHandler;
+import reactor.netty.http.brave.ReactorNettyHttpTracing;
 import reactor.netty.http.client.HttpClient;
 
 @Configuration
 public class Config {
 
   @Bean
-  WebClient webClient(Logbook logbook) {
+  NettyServerCustomizer nettyServerCustomizer(
+      Logbook logbook, ReactorNettyHttpTracing reactorNettyHttpTracing) {
+    return server ->
+        reactorNettyHttpTracing.decorateHttpServer(
+            server.doOnConnection(conn -> conn.addHandlerFirst(new LogbookServerHandler(logbook))));
+  }
+
+  @Bean
+  ReactorNettyHttpTracing reactorNettyHttpTracing(HttpTracing httpTracing) {
+    return ReactorNettyHttpTracing.create(httpTracing);
+  }
+
+  @Bean
+  WebClient webClient(Logbook logbook, ReactorNettyHttpTracing tracing) {
     return WebClient.builder()
         .baseUrl("https://randomuser.me")
         .clientConnector(
             new ReactorClientHttpConnector(
-                HttpClient.create()
-                    .doOnConnected(conn -> conn.addHandlerLast(new LogbookClientHandler(logbook)))))
+                tracing.decorateHttpClient(
+                    HttpClient.create()
+                        .doOnConnected(
+                            conn -> conn.addHandlerLast(new LogbookClientHandler(logbook))))))
         .build();
   }
 }
